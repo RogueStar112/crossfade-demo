643b8f7a279e17a3480da6478f9c27fe4ec08a6e
DMistica <hildtgunnt@gmail.com>
2023-11-13 13:55:42 +0000

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import AudioComponent from './AudioComponent'
+
+
+function App() {
+
+  return (
+    <>  
+      <AudioComponent />
+    </>
+  )
+}
+
+export default App
