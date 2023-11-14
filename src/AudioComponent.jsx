cd7843146fdec3a65cc2d606166ccb295bec1aa6
DMistica <hildtgunnt@gmail.com>
2023-11-14 02:22:19 +0000

    Crossfade btn update

--- src/AudioComponent.jsx
+++ src/AudioComponent.jsx
@@ -1,25 +1,56 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const AudioComponent = () => {
   const audioContextRef = useRef(null);
+  const source1Ref = useRef(null);
+  const gainNode1Ref = useRef(null);
+  
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audioBufferSource, setAudioBufferSource] = useState(null);
+  const [volume, setVolume] = useState(1);
 
+  
+
+
+  // First use effect for initializing audio.
   useEffect(() => {
-    // Create an AudioContext when the component mounts
+    
     audioContextRef.current = new (window.AudioContext || window.webkitAudioContext)();
+
+  });
+
+  // Second use effect 
+  useEffect(() => {
+
+    console.log("ACR CURRENT 1147", audioContextRef.current)
+
+    // const audio = audioContextRef.current;
+
+    // const gainNode = audio.createGain();
+
+    // gainNode.gain.value = volume;
+
+    // audio.volume = volume;
+
+    // console.log('VOLUME' , volume)
 
     // Clean up the audio context when the component unmounts
     return () => {
-      if (audioContextRef.current && audioContextRef.current.state === 'running') {
-        audioContextRef.current.close();
-      }
+      // if (audioContextRef.current && audioContextRef.current.state === 'running') {
+      //   audioContextRef.current.close();
+      // }
     };
-  }, []);
+  }, [volume]);
+
+
+  const updateVolume = async (audio_source) => {
+
+    audio_source.gain.value = volume
+
+  }
 
   const handleClick = async () => {
     // Check if the AudioContext is in a suspended state and resume it
-    if (audioContextRef.current.state === 'suspended') {
-      await audioContextRef.current.resume();
-    }
 
     // Fetch and decode an audio file
     const response1 = await fetch('./src/assets/music/chainsmokers_thisfeeling.mp3');
@@ -31,25 +62,68 @@ const AudioComponent = () => {
     const arrayBuffer2 = await response2.arrayBuffer();
     const audioBuffer2 = await audioContextRef.current.decodeAudioData(arrayBuffer2);
 
-    // Create BufferSourceNodes for each audio file
+    // Create BufferSourceNodes and GainNode for each audio file
     const source1 = audioContextRef.current.createBufferSource();
-    source1.buffer = audioBuffer1;
+    const gainNode1 = audioContextRef.current.createGain();
+    source1.connect(gainNode1);
+
+    gainNode1.gain.value = volume
+    gainNode1Ref.current = gainNode1;
+    source1Ref.current = source1;
+
 
     const source2 = audioContextRef.current.createBufferSource();
-    source2.buffer = audioBuffer2;
-
-    // Connect sources to the destination (speakers)
-    source1.connect(audioContextRef.current.destination);
     source2.connect(audioContextRef.current.destination);
+
+    // Connect GainNode to the destination (speakers)
+    gainNode1.connect(audioContextRef.current.destination);
+
+     
+
+    // Set the initial volume for source1
+    gainNode1.gain.value = volume;
+
+    // Set the audio buffers for each source
+    source1.buffer = audioBuffer1;
+    source2.buffer = audioBuffer2;
 
     // Start both sources simultaneously
     source1.start();
     source2.start();
-  };
+};
+
+  const handlePauseResume = async () => {
+    
+    if (audioContextRef.current.state === 'suspended') {
+      await audioContextRef.current.resume().then(function () {
+        document.getElementById('pause-resume-btn').textContent = 'Pause';
+      });
+    } else if (audioContextRef.current.state === 'running') {
+      await audioContextRef.current.suspend().then(function () {
+        document.getElementById('pause-resume-btn').textContent = 'Resume';
+      });
+    }
+
+  }
+
+  const handleCrossfade = async () => {
+
+    const interval = setInterval(() => {
+      if (gainNode1Ref.current) {
+        const currentTime = audioContextRef.current.currentTime;
+        gainNode1Ref.current.gain.linearRampToValueAtTime(0, currentTime + 3); // Decrease volume to 0 over 3 seconds
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+  }
 
   return (
     <div>
       <button onClick={handleClick}>Play Audio</button>
+      <button onClick={handlePauseResume} id="pause-resume-btn">Pause</button>
+      <button onClick={handleCrossfade}>Crossfade</button>
     </div>
   );
 };
